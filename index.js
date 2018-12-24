8ec4399bdc64ff3409d52815df9e0583b824568c
Luis Campos <lcamposguajardo@salesforce.com>
2018-12-23 17:55:59 -0800

    Rewrite commands to support new setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const { https_get, download, extract, installExtension } = require('./reqmod');
+const fs = require('fs');
 const program = require('commander');
 const chalk = require('chalk');
 const url = require('url');
@@ -15,13 +16,65 @@ let buildValue;
 
 program
 .version('0.1.1')
-.arguments('<build>') // if nothing is set, the whole thing fails
+/* .arguments('<build>') // if nothing is set, the whole thing fails
 .action(function(build) {
   console.log('action piece, build ', build);
   buildValue = build;
 })
-.parse(process.argv);
+.parse(process.argv); */
 
+program
+  .command('setup [env]')
+  .description('Create the configuration needed to run this cli')
+  .option('-t, --token', 'CircleCI personal API token you created')
+  .option('-v, --vcs_type', 'The version control system (VCS) you are using. Either github or bitbucket.')
+  .option('-u, --username', 'The VCS project account username or organization name for the target project. Located at the top left of the screen in the CircleCI application.')
+  .option('-p, --project', 'The name of the target VCS repository.')
+  .option('-m, --publisher', 'VSCode marketplace publisher name.')
+  .action(function(env, options){
+    //console.log('----- options = ', options);
+    try {
+      if (!options.token) {
+        throw new Error('--token is required')
+      }
+
+      const vcsType = options.vcs_type || 'github';
+      if (!options.vcs_type) {
+        console.log('Defaulting version control system setting to github');
+      }
+
+      if (!options.username) {
+        throw new Error('--username is required')
+      }
+
+      if (!options.project) {
+        throw new Error('--project is required')
+      }
+
+      if (!options.publisher) {
+        throw new Error('--publisher is required')
+      }
+
+      const inputConfig = {
+        CIRCLE_TOKEN: options.token,
+        vcs_type: vcsType,
+        "username": options.username,
+        "project": options.project,
+        "vscode_publisher": options.publisher
+      }
+
+      fs.writeFileSync('./config.json', JSON.stringify(inputConfig, null, 2));
+    } catch (e) {
+      console.log('There was an error creating the cli configuration file.', e);
+      process.exit(1);
+    }
+    console.log('The cli was successfully configured.');
+  });
+
+  program.parse(process.argv);
+ 
+console.log('stuff');
+  /*
 const last_builds_opts = {
   host: 'circleci.com',
   path: encodeURI(`/api/v1.1/project/${vcs_type}/${username}/${project}?circle-token=${CIRCLE_TOKEN}&limit=10&offset=0&filter=completed`),
@@ -97,4 +150,4 @@ try {
 
 } catch (err) {
   console.error(chalk.red(err));
-}
+} */
